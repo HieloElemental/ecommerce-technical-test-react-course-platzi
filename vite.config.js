fc168b4ad9ae1b0d50d9a7b4cedd0414727ab326
Hielo Elemental <hieloelemental@gmail.com>
2024-09-08 21:09:31 -0500

    refactor(repositoty refactor): :recycle: refactor to te project due to the standardization of my github repos

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Prueba-Tecnica-E-commerce-Profesional-React-Platzi/",
+  base: "/ecommerce-technical-test-react-course-platzi/",
 });
